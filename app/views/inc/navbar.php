06462cc8f55cf45527af53a0b59852f8f2bba25c
viwdya <viviwidi10@gmail.com>
2025-06-09 21:56:19 +0700

    memperbaiki tampilan web
    memperbaiki tampilan web agar lebih enak dilihat

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,19 +1,18 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4 shadow">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand font-weight-bold" href="<?php echo URLROOT; ?>">CMS Sederhana</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/cms_sederhana/' ? 'active' : ''); ?>">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?php echo (strpos($_SERVER['REQUEST_URI'], '/posts') !== false ? 'active' : ''); ?>">
                     <a class="nav-link" href="<?php echo URLROOT; ?>/posts">Posts</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item <?php echo (strpos($_SERVER['REQUEST_URI'], '/pages/about') !== false ? 'active' : ''); ?>">
                     <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
                 </li>
             </ul>
